aab742dcabe55b8a9225bda94470ba52fbabc3b3
clemence-p <clemenceplourde@yahoo.ca>
2025-12-06 16:15:47 -0500

    Add files via upload

--- interface.py
+++ interface.py
@@ -1,14 +1,49 @@
 import spreadsheet
 import codeboot
+from pymsgbox import prompt
 
 # Initialise l'interface graphique, crée les éléments HTML nécessaires, lie les
 # évènements aux fonctions appropriées, etc.
 def init():
+    sheet = document.querySelector('#spreadsheet')  # accéder à l'élément #spreadsheet
+    sheet.innerHTML = "<p>blalabl</p>"     #changer le contenu de spreadsheet, on va vouloir mettre la table ici avec <tr>, <tb>
+
+    codeboot.add_file_drop_handler(document.querySelector('#cb-body'), drop_file)
+
+    
+# il faut se définir une équivalence entre une matrice et la table html qui est affichée.
+
+current_sheet = matrices(40,20,'')   
+
+
+def read_csv(path):
     pass
+
+def write_csv(path):
+    pass
+
+def table(cont): return '<table>' + cont + '</table>'
+
+def tr(cont): return '<tr>' + cont + '</tr>'
+
+def th(cont): return '<th>' + cont + '</th>'
+
+def td(cont): return '<td>' + cont + '</td>'        
+
+def cell(row_idx, col_idx):
+    selector = f'[data-row="{row_idx}"][data-col="{col_idx}"]'
+    return document.querySelector(selector)
+
+selected_cell = None
 
 # Gère  l'évènement de clic sur une cellule située à la position 
 # (row_idx, col_idx) dans la table.
-def cell_clicked(row_idx, col_idx):
+def cell_clicked(row_idx, col_idx):          # à compléter
+    global selected_cell
+    if selected_cell != None:
+        selected_cell.classList.remove('selected')
+    selected_cell = cell(row_idx, col_idx)
+    selected_cell.classList.add('selected')
     pass
 
 # Gère l'évènement de téléversement (drag-and-drop) d'un fichier CSV. Le
@@ -17,6 +52,10 @@ def cell_clicked(row_idx, col_idx):
 # zone de téléversement dans l'interface graphique, qui est simplement tout le
 # body de la page.
 def drop_file(files):
+    name = files.filename
+    content = files.content
+    lines = content.split('\n')         # pas finit, juste un début de l'idée de ce qu'il faut faire
+    # on veut lire le content et l'afficher dans une table appropriée.
     pass
 
 # Gère l'évènement de pression d'une touche dans le champ de texte utilisé pour
@@ -30,6 +69,9 @@ def cell_editor_pressed(event):
 # créer une nouvelle table vide et l'afficher dans l'interface graphique. Une 
 # table vide contient par définitions 20 colonnes et 40 lignes.
 def new_sheet_button_clicked():
+    ligne = '<tr>' + '<td></td>'*20 + '</tr>'            # est-ce que les cells crées devraient être identifié par le selector comme décrit dans la fct cell?
+    tab = 40*ligne
+    return table(tab)
     pass
 
 # Gère l'évènement de clic sur le bouton "Sauvegarder table". Cette fonction 
@@ -37,6 +79,9 @@ def new_sheet_button_clicked():
 # fichier CSV, et sauvegarder les données actuelles dans ce fichier. La ligne
 # qui contient les résultats des calculs ne doit pas être sauvegardée.
 def save_sheet_button_clicked():
+    nom_fichier = prompt('Nom du fichier')
+    # quelque chose qui transforme la table de données actuelles en csv : write csv
+    # utiliser codeboot.download('nomfichier', text)   oû text peut être la valeur d'une fonction
     pass
 
 # Gère l'évènement de clic sur le bouton "Ajouter ligne avant". Cette fonction
@@ -62,6 +107,7 @@ def add_column_after_button_clicked():
 # Gère l'évènement de clic sur le bouton "Supprimer ligne". Cette fonction
 # doit supprimer la ligne sélectionnée dans la table.
 def delete_row_button_clicked():
+
     pass
 
 # Gère l'évènement de clic sur le bouton "Supprimer colonne". Cette fonction
